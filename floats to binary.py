11638e4ea1fd6b9b92abca28fc46dfe87f5c07ef
Priya Bharti <31110570+priya-bharti@users.noreply.github.com>
2018-02-03 13:00:58 +0530

    Create floats to binary.py

---
+++ floats to binary.py
@@ -0,0 +1,19 @@
+num = float(input("Enter a number between 0 and 1: "))
+p = 0
+binary = ''
+
+while ((2**p)*num)%1 != 0:
+    p += 1
+
+num2 = int((2**p)*num)
+
+while num2 > 0:
+    binary = str(num2%2) + binary
+    num2 = num2//2
+
+print(binary)
+
+for i in range (p - len(binary)):
+    binary = '0' + binary
+
+print('Binary equivalent: ', binary[0:-p], '.', binary[-p:])
